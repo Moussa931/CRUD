4cf76ebc1824afb470a6cd5fa6f344f49b2d07d7
Moussa931 <123644909+Moussa931@users.noreply.github.com>
2024-04-23 04:08:05 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongo Connecté");
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+};
+
+module.exports = connectDB;
 
